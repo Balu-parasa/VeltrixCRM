177ce5bf8ba0d21978083f87a6001ee0aaecbf9b
Parasa Balu Lakshman Pavan <baluparasa3@gmail.com>
2026-08-29 23:01:20 +0530

    Add Mailables traits to ContactInquiry class

--- app/Mail/ContactInquiry.php
+++ app/Mail/ContactInquiry.php
@@ -1,7 +1,11 @@
 <?php
 
 namespace App\Mail;
-
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
 
 
 class ContactInquiry extends Mailable
